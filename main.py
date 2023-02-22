857798ed3d1fb662f8da3315fbdbe4641b81f397
matej <matej.music@gmail.com>
2023-02-22 21:02:53 +0100

    Added some exercises.

--- main.py
+++ main.py
@@ -2,20 +2,30 @@
 # by Matej Music 2023
 # ------------------------------------
 
-todos = []
 
 while True:
     user_action = input('Type add, show, edit, complete or exit(x): ')
     user_action = user_action.strip()
     match user_action:
         case 'add':
-            todo = input('Enter a TO-DO: ')
+            todo = input('Enter a TO-DO: ') + '\n'
+
+            file = open('todo-data.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             todos.append(todo)
+            file = open('todo-data.txt', 'w')
+            file.writelines(todos)
+            file.close()
         case 'show':
+            file = open('todo-data.txt', 'r')
+            todos = file.readlines()
             for index, item in enumerate(todos):
                 item = item.title()
                 row = f'{index + 1}-{item}'
                 print(row)
+            file.close()
         case 'edit':
             number = int(input('Enter the number of TODO to edit: '))
             number -= 1
